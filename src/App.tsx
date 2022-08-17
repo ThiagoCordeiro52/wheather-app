aed9b729e9e62e6b205f399178c26c0a4c2ce040
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-08-17 14:05:01 -0300

    :recycle: refactor: moved form from app component to form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { useState } from 'react';
+import { Form } from './components/Form';
 
 function App() {
-  return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <form>
-        <input
-          type="text"
-          placeholder="cidade"
-          className="p-3 rounded-lg outline-none"
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
-        >
-          Pesquisar
-        </button>
-      </form>
-    </div>
-  );
+  return <Form />;
 }
 
 export default App;
