80af2cf640b0c1ca4e55301f386103aa33dae4d2
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-08-17 14:36:41 -0300

    :sparkles: feat: styles added to card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Card } from './components/Card';
 import { Form } from './components/Form';
 
 function App() {
-  return <Form />;
+  return (
+    <div className="flex flex-col w-full h-screen items-center justify-center">
+      <Form />
+      <Card />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,17 @@
 export function Form() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <form>
-        <input
-          type="text"
-          placeholder="cidade"
-          className="p-3 rounded-lg outline-none"
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
-        >
-          Pesquisar
-        </button>
-      </form>
-    </div>
+    <form>
+      <input
+        type="text"
+        placeholder="cidade"
+        className="p-3 rounded-lg outline-none"
+      />
+      <button
+        type="submit"
+        className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
+      >
+        Pesquisar
+      </button>
+    </form>
   );
 }
